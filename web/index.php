5325e7c4013a76a8da1fff1d3a8778215dcf696b
Ricardo Pérez <ricpelo@gmail.com>
2018-02-13 14:43:21 +0100

    Usa symfony/dotenv

--- web/index.php
+++ web/index.php
@@ -1,9 +1,15 @@
 <?php
 
-define('YII_ENV', getenv('YII_ENV') ?: 'dev');
+require __DIR__ . '/../vendor/autoload.php';
+
+if (($getenv = getenv('YII_ENV')) !== 'prod'
+    && file_exists($file = __DIR__ . '/../.env')) {
+    (new \Symfony\Component\Dotenv\Dotenv())->load($file);
+}
+
+define('YII_ENV', $getenv ?: 'dev');
 define('YII_DEBUG', getenv('YII_DEBUG') ?: YII_ENV == 'dev');
 
-require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';

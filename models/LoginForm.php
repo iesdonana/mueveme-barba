3c58db9b444fe58b327252c6b0ab8e4a363df6c7
Arturo Barba <arturo.barba@iesdonana.org>
2019-02-01 18:54:10 +0100

    Logueo y logout usuarios

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -9,14 +9,12 @@ use yii\base\Model;
  * LoginForm is the model behind the login form.
  *
  * @property User|null $user This property is read-only.
- *
  */
 class LoginForm extends Model
 {
     public $username;
     public $password;
     public $rememberMe = true;
-
     private $_user = false;
 
 
@@ -48,6 +46,7 @@ class LoginForm extends Model
             $user = $this->getUser();
 
             if (!$user || !$user->validatePassword($this->password)) {
+                echo var_dump($user);
                 $this->addError($attribute, 'Incorrect username or password.');
             }
         }
@@ -60,20 +59,20 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
         return false;
     }
 
     /**
-     * Finds user by [[username]]
+     * Finds user by [[username]].
      *
      * @return User|null
      */
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = Usuarios::find()->where(['nombre' => $this->username])->one();
         }
 
         return $this->_user;
--- models/Usuarios.php
+++ models/Usuarios.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "usuarios".
@@ -14,7 +15,7 @@ use Yii;
  * @property Comentarios[] $comentarios
  * @property Noticias[] $noticias
  */
-class Usuarios extends \yii\db\ActiveRecord
+class Usuarios extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -63,5 +64,57 @@ class Usuarios extends \yii\db\ActiveRecord
     public function getNoticias()
     {
         return $this->hasMany(Noticias::className(), ['usuario_id' => 'id'])->inverseOf('usuario');
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     *
+     * @param string|int $id the ID to be looked for
+     * @return IdentityInterface|null the identity object that matches the given ID.
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     *
+     * @param string $token the token to be looked for
+     * @param null|mixed $type
+     * @return IdentityInterface|null the identity object that matches the given token.
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+    }
+    /**
+     * @return int|string current user ID
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @return string current user auth key
+     */
+    public function getAuthKey()
+    {
+    }
+    /**
+     * @param string $authKey
+     * @return bool if auth key is valid for current user
+     */
+    public function validateAuthKey($authKey)
+    {
+    }
+    /**
+     * Validates password.
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password);
     }
 }

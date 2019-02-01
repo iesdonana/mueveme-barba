602fe6963d427b60ac162be2b2087e1ae0eb1074
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-01 19:34:11 +0100

    Vista inicial de una noticia

--- controllers/NoticiasController.php
+++ controllers/NoticiasController.php
@@ -2,12 +2,12 @@
 
 namespace app\controllers;
 
-use Yii;
 use app\models\Noticias;
 use app\models\NoticiasSearch;
+use Yii;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 
 /**
  * NoticiasController implements the CRUD actions for Noticias model.
@@ -46,7 +46,7 @@ class NoticiasController extends Controller
 
     /**
      * Displays a single Noticias model.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -78,7 +78,7 @@ class NoticiasController extends Controller
     /**
      * Updates an existing Noticias model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -98,7 +98,7 @@ class NoticiasController extends Controller
     /**
      * Deletes an existing Noticias model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -112,7 +112,7 @@ class NoticiasController extends Controller
     /**
      * Finds the Noticias model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
+     * @param int $id
      * @return Noticias the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
--- views/noticias/view.php
+++ views/noticias/view.php
@@ -6,36 +6,24 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Noticias */
 
-$this->title = $model->id;
+$this->title = $model->titulo;
 $this->params['breadcrumbs'][] = ['label' => 'Noticias', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="noticias-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::a(Html::encode($this->title), Html::encode($model->noticia)) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <?php
+    $inicio = strpos($model->noticia, "//")+2;
+    $fin = strpos($model->noticia, ".", $inicio)-$inicio;
+    $str = mb_substr($model->noticia, $inicio, $fin);
+    ?>
+    <small>por <?= $model->usuario->nombre ?> a <?= Html::a($str, $model->noticia) ?>  </small><br>
+    <!-- Falta fecha de noticias -->
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'titulo',
-            'cuerpo:ntext',
-            'noticia',
-            'categoria_id',
-            'usuario_id',
-        ],
-    ]) ?>
+    <?= Html::encode($model->cuerpo) ?>
+
 
 </div>

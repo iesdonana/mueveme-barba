abfbfccab97539a7d58383e282475cc8af05f296
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-10 18:57:37 +0100

    Visualizacion de comentarios sin anidamiento

--- views/comentarios/_smallView.php
+++ views/comentarios/_smallView.php
@@ -1,20 +1,36 @@
 <?php
 use yii\helpers\Html;
+$formatter = \Yii::$app->formatter;
 ?>
+<style media="screen">
+.comentario {
+    padding: 5px;
+}
+.comentario-cuerpo {
+    background-color: #fff5ed;
+    position: relative;
+    padding: 5px 10px 5px 30px;
+    border-radius: 2px;
+}
+.comentario-texto {
+    padding-left: 10px;
+}
+.votos {
+    padding-left: 10px;
+    padding-top: 3px;
+}
+</style>
 
-<div class="comentarios-view">
-
-    <?php
-    $formatter = \Yii::$app->formatter;
-    ?>
-    <br>
-    <?= Html::encode($model->cuerpo) ?>
-    <br>
-    <small>
-        por <?= Html::a(Html::encode($model->usuario->nombre),
+<div class="comentario">
+    <div class="comentario-cuerpo">
+        <div class="comentario-texto">
+            <?= Html::encode($model->cuerpo) ?>
+            <br>
+            <small>
+                por <?= Html::a(Html::encode($model->usuario->nombre),
                 ['usuarios/view', 'id' => $model->usuario_id]) ?> -----
-        Creado a <?= $formatter->asTime($model->created_at, 'short') ?> <?= $formatter->asRelativeTime($model->created_at, new DateTime()) ?>
-    </small><br>
-
-
+                Creado a <?= $formatter->asTime($model->created_at, 'short') ?> <?= $formatter->asRelativeTime($model->created_at, new DateTime()) ?>
+            </small>
+        </div>
+    </div>
 </div>

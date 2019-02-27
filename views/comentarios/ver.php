add607374ab5fcc7c9122a89af67274bc0a8497d
Arturo Barba <arturo.barba@iesdonana.org>
2019-02-27 20:55:29 +0100

    Corrección de errores

--- views/comentarios/ver.php
+++ views/comentarios/ver.php
@@ -50,7 +50,6 @@ $this->params['breadcrumbs'][] = $this->title;
         }
         ?>
     </div>
-    <?php echo $model->id ?>
 
     <?= Html::a('Comentar', ['comentarios/create', 'noticia_id' => $model->id], ['class' => 'btn btn-primary']) ?>
 </div>
